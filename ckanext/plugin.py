18ca31dc4bf763824fa1014f0a34276bb23d1281
Poguez <zeugop@gmail.com>
2014-07-04 00:43:23 -0500

    Added plugin

---
+++ ckanext/plugin.py
@@ -0,0 +1,24 @@
+import logging
+import ckan.plugins as p
+import ckan.plugins.toolkit as toolkit
+
+
+log = logging.getLogger(__name__)
+
+
+class WidgetsPlugin(p.SingletonPlugin):
+    p.implements(p.IRoutes, inherit=True)
+    p.implements(p.IConfigurer, inherit=True)
+
+    def before_map(self, map):
+        #This is a reference to the controller.
+        widgets_controller = 'ckanext.widgets.controller:WidgetsController'
+        map.connect('/dataset/{id}/widget', controller=widgets_controller, action='view_widget')
+        return map
+
+    def update_config(self, config):
+        #This plugin's templates.
+        toolkit.add_template_directory(config, 'templates')
+
+        #This plugin public files.
+        toolkit.add_public_directory(config, 'public')
