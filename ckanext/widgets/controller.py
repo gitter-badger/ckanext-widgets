cd7c5a8578b5b77c5643bcf30159cdf3c919e201
Miguel Angel Gordian <miguel.angel@codeandomexico.org>
2014-07-08 18:50:11 +0000

    Add context and variables for the template 'widget.html'
    Signed-off-by: Miguel Angel Gordian <miguel.angel@codeandomexico.org>

--- ckanext/widgets/controller.py
+++ ckanext/widgets/controller.py
@@ -6,6 +6,23 @@ _ = p.toolkit._
 class WidgetsController(p.toolkit.BaseController):
     controller = 'ckanext.widgets.controller.WidgetsController'
 
-    #@p.toolkit.auth_allow_anonymous_access
-    def view_widget(self):
-        return p.toolkit.render('widget.html')
+    def view_widget(self, id):
+        '''
+        Embeded page for  widget visualization.
+        '''
+        # We need to investigate more about context
+        context = {
+            #'model': model, 'session': model.Session,
+            #'user': c.user or c.author, 'auth_user_obj': c.userobj
+        }
+        
+        try:
+            c.package = get_action('package_show')(context, {'id': id})
+            data_dict = {'resource': c.resource, 'package': c.package}
+        except NotFound:
+            abort(404, _('Resource not found'))
+        except NotAuthorized:
+            abort(401, _('Unauthorized to read resource %s') % id)
+        
+        return p.toolkit.render('widget.html', data_dict)
+
