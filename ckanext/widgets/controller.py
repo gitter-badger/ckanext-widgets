1b133ece558deb22d8001df2b6e2f4ed31132799
poguez <zeugop@gmail.com>
2014-07-15 17:33:38 -0500

    Exceptions now raise correctly and changed the import of the library. Now we import exceptions from ckan.plugins.toolkit

--- ckanext/widgets/controller.py
+++ ckanext/widgets/controller.py
@@ -1,13 +1,8 @@
 import ckan.plugins as p
 import logging
-import ckan.logic as logic
+from ckan.plugins.toolkit import (NotAuthorized, get_action, abort, _, c,
+ObjectNotFound as NotFound)
 
-NotFound = logic.NotFound
-NotAuthorized = logic.NotAuthorized
-get_action = p.toolkit.get_action
-
-_ = p.toolkit._
-c = p.toolkit.c
 
 class WidgetsController(p.toolkit.BaseController):
     controller = 'ckanext.widgets.controller.WidgetsController'
@@ -21,14 +16,13 @@ class WidgetsController(p.toolkit.BaseController):
             #'model': model, 'session': model.Session,
             #'user': c.user or c.author, 'auth_user_obj': c.userobj
         }
-        
         try:
             c.package = get_action('package_show')(context, {'id': id})
             data_dict = {'resource': c.resource, 'package': c.package}
+            return p.toolkit.render('widget.html', data_dict)
         except NotFound:
             abort(404, _('Resource not found'))
         except NotAuthorized:
             abort(401, _('Unauthorized to read resource %s') % id)
-        
-        return p.toolkit.render('widget.html', data_dict)
-
+        except:
+            abort(500, _('There was an internal error %s') % id)
